0a44515a6973fd7eb13f29596ab661bd6ca609bb
maiya-22 <maiya_o@yahoo.com>
2018-06-26 12:15:35 -0400

    image added to readme

---
+++ readme.md
@@ -0,0 +1,31 @@
+#bare-bones Express and Node.js
+
+### This is a little app to help someone get oriented to the basic express set-up:
+
+## Steps for set-up:
+
+1.  - Have Node.js installed (I had version 10.4.0 for making this, but don't think it matters)
+2.  - In terminal, run "npm install" inside of the root folder.
+      ## (This command will create a node_modules folder in the root directory, read the package-lock.json and install whatever modules the project requires).
+3.  - Start the server by typing "node server" in the terminal (the .js extension is default. That's why youd on't have to type node server.js)
+
+## File-structure:
+
+*Your server is configured and running in "server.js"
+*You have a node module exporting an object as "data.js", which is being required into the "server.js"
+*The "server.js" is also able to serve up the "index.html"
+*The package.json is where you have info about your project (even the creater of node said that this file has too much unnecessary info). The "scripts" property is useful, though.
+
+Instructions:
+
+1.  look at the routes in the server.js file
+2.  use postman and/or your browser to play with the get requests
+3.  use postman to play around with the post request
+
+## put any query-string in the url bar and see what you get back. Notice how express is giving this data back to you.
+
+## put any keys and values in the body (instead of a form). Notice how express is giving this data back to you
+
+## Sample Postman requests:
+
+![sample postman](http://git_hub/postman.png)
--- server.js
+++ server.js
@@ -1,14 +1,47 @@
 const express = require("express"); //express is a node module, so no need to put a file-path
 const app = express(); //the express module is a function that returns an object.  When we invoke express, we now have an object with a bunch of methods.  Those methods configure our routes and set up our server to listen on a port, etc.
+const bodyParser = require("body-parser");
 const data = require("./data"); //this is not a node module, it's a module that we wrote, so it has ./ in front to signal to look via a file-path
+
+// .use method runs on every single request.  That is why it's called 'middleware'
+//  the request is just a string via the http protocol
+// this function parses the 'body' of the request or 'payload' into a request.body object
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 // express makes an object that allows us to access things on the request object.  Then, we attatch whatever we want to the response object and send it back to the server.
 app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/index.html", function(error) {
+    if (error) {
+      response.status(500).send(error);
+    }
+  });
+});
+
+app.get("/hello-world", (request, response) => {
   response.send("hello");
 });
 
 app.get("/data", (request, response) => {
   response.json(data);
+});
+
+// go to postman, and run some random post requests to your port:
+// put various things in the keys and the values, and/or send a query string
+app.post("/", (request, response) => {
+  console.log(request.body);
+  const body = request.body; //es6 destructuring. body = request.body
+  const { query } = request; //es6 destructuring. body = request.query
+  // this is also es6 short-hand for sampleData = {body: body, query: query}
+  const sampleData = {
+    body: body,
+    query: query || "query is default, so you will never see this?"
+  };
+  // the response will return your request query and parameter to you, so you can see how express structures them:
+  response.json(sampleData);
 });
 
 const PORT = 3000;
